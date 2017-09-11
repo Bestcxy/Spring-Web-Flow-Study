09382fb26da8f04cac24363ab09f10deaeff4f94
wujie <snailknight@yeah.net>
2017-09-11 22:51:31 +0800

    Spring Web Flow简单实现：http://localhost:8085/springwebflow/index.jsp

---
+++ src/main/java/com/bestcxx/stu/springwebflow/bean/Payment.java
@@ -0,0 +1,12 @@
+package com.bestcxx.stu.springwebflow.bean;
+
+import java.io.Serializable;
+
+public class Payment implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7702356545180109916L;
+
+}
